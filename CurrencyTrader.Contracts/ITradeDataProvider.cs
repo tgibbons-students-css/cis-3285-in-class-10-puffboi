ae3fb0a77c8f26609037dfbaa82dccd95ddf3288
puffboi <spohlmann@css.edu>
2016-11-10 11:29:25 -0600

    done

--- CurrencyTrader.Contracts/ITradeDataProvider.cs
+++ CurrencyTrader.Contracts/ITradeDataProvider.cs
@@ -5,5 +5,6 @@ namespace CurrencyTrader.Contracts
     public interface ITradeDataProvider
     {
         IEnumerable<string> GetTradeData();
+        
     }
 }
--- CurrencyTrader/AsynchUrlTradeDataProvider.cs
+++ CurrencyTrader/AsynchUrlTradeDataProvider.cs
@@ -2,26 +2,38 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CurrencyTrader
 {
-    public class AsynchUrlTradeDataProvider : ITradeDataProvider
+    public class AsynchUrlTradeDataProvider : IAsynchTradeDataProvider
     {
         private readonly String url;
         UrlTradeDataProvider SynchTradeProvider;
+        static TradeDataUpdate tradeUpdater;
 
-        public AsynchUrlTradeDataProvider(String url)
+        public AsynchUrlTradeDataProvider(String url, TradeDataUpdate newtradeUpdater)
         {
             this.url = url;
+            tradeUpdater = newtradeUpdater;
             SynchTradeProvider = new UrlTradeDataProvider(url);
         }
-        public IEnumerable<string> GetTradeData()
+        public void GetTradeData()
         {
-            Task.Run(() => SynchTradeProvider.GetTradeData());
-            //return SynchTradeProvider.GetTradeData();
+            //Task.Run(() => SynchTradeProvider.GetTradeData());
+            WebClient client = new WebClient();
+            Uri uri = new Uri(url);
+            client.DownloadStringCompleted += DownloadStringCompleted;
+            client.DownloadStringAsync(new Uri(url));
 
+        }
+        static void DownloadStringCompleted(object sender,
+            DownloadStringCompletedEventArgs e)
+        {
+            string[] lines = e.Result.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            tradeUpdater.UpdateTradeData(lines);
         }
     }
 }
